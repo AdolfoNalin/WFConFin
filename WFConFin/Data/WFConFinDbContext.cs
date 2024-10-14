9edf21ba0d187de9b0a47dd1484150d53fdfcd31
AdolfoNalin <nalinadolfo33@gmail.com>
2024-10-14 12:40:02 -0300

    Criando arquivos de contexto

---
+++ WFConFin/Data/WFConFinDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Data;
+using WFConFin.Models;
+
+namespace WFConFin.Data
+{
+    public class WFConFinDbContext : DbContext
+    {
+        public DbSet<User> User { get; set; }
+        public DbSet<Account> Account{ get; set; }
+        public DbSet<State> State { get; set; }
+        public DbSet<City> City { get; set; }
+        public DbSet<Persona> Persona { get; set; }
+
+        public WFConFinDbContext(DbContextOptions<WFConFinDbContext> options) : base(options)
+        {
+            
+        }
+    }
+}

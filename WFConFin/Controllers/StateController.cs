2d2e2685809985e215c69720a17672eba0ea20dc
AdolfoNalin <nalinadolfo33@gmail.com>
2024-10-14 14:44:06 -0300

    Passando os valores State.Name and State.Sigla Para ToUpper() nos métodos Get, Post, Put
    Criando os métodos GetSiglaSmart, GetSigla,GetStatePagination

--- WFConFin/Controllers/StateController.cs
+++ WFConFin/Controllers/StateController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
 using System.Reflection.Metadata.Ecma335;
 using System.Resources;
 using WFConFin.Data;
@@ -17,6 +18,104 @@ namespace WFConFin.Controllers
         {
             this._context = context;
         }
+
+        #region GetSigla
+        [HttpGet("{sigla}")]
+        public IActionResult GetSigla([FromRoute] string sigla)
+        {
+            try
+            {
+                sigla = sigla.ToUpper();
+                State state = _context.State.Find(sigla) ?? throw new NullReferenceException($"Estado com a sigla {sigla} não existe no banco de dados");
+                return Ok(state);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Error in GetSigla With message: {ex.Message} with path: {ex.StackTrace}");
+            }
+        }
+        #endregion
+
+        #region GetSiglaSmart
+        [HttpGet("Search")]
+        public IActionResult GetSiglaSeach([FromQuery] string value)
+        {
+            try
+            {
+                value = value.ToUpper();
+
+                //Entity
+                var state = _context.State.Where(s => s.Name.ToUpper().Contains(value) || s.Sigla.ToUpper().Contains(value)).ToList();
+
+                /*Query criteria
+                var list = from o in _context.State.ToList()
+                           where o.Name.ToUpper().Contains(value) || o.Sigla.ToUpper().Contains(value)
+                           select o;*/
+
+                //Expression
+                /*Expression<Func<State, bool>> stateExpression = o => true;
+                stateExpression = o => o.Name.ToUpper().Contains(value) || o.Sigla.ToUpper().Contains(value);
+
+                List<State> state = _context.State.Where(stateExpression).ToList();*/
+
+                _ = state.FirstOrDefault() ?? throw new NullReferenceException($"Estado não existe no banco de dados");
+
+                return Ok(state);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Error in GetSiglaSearch With message: {ex.Message} with path: {ex.StackTrace}");
+            }
+        }
+        #endregion
+
+        #region GetSiglaPagination
+        [HttpGet("Pagination")]
+        public IActionResult GetStatePagination([FromQuery] string value, int skip, int take, bool desc)
+        {
+            try
+            {
+                value = value.ToUpper();
+
+                //Entity
+                //var state = _context.State.Where(s => s.Name.ToUpper().Contains(value) || s.Sigla.ToUpper().Contains(value)).ToList();
+
+                //Query criteria
+                var state = from o in _context.State.ToList()
+                           where o.Name.ToUpper().Contains(value) || o.Sigla.ToUpper().Contains(value)
+                           select o;
+
+                //Expression
+                /*Expression<Func<State, bool>> stateExpression = o => true;
+                stateExpression = o => o.Name.ToUpper().Contains(value) || o.Sigla.ToUpper().Contains(value);*/
+
+                //var state = _context.State.Where(stateExpression).ToList();
+
+                _ = state.FirstOrDefault() ?? throw new NullReferenceException($"Estado não existe no banco de dados");
+
+                if (desc)
+                {
+                    state = from o in state orderby o.Name descending select o;
+                }
+                else
+                {
+                    state = from o in state orderby o.Name ascending select o;
+                }
+
+                int amount = state.Count();
+
+                state = state.Skip(skip).Take(take).ToList();
+
+                var pr = new PaginationResponse<State>(state, amount, skip, take);
+
+                return Ok(pr);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Error in GetStatePagination With message: {ex.Message} with path: {ex.StackTrace}");
+            }
+        }
+        #endregion
 
         #region GetAll
         [HttpGet]
@@ -39,6 +138,8 @@ namespace WFConFin.Controllers
         {
             try
             {
+                state.Name = state.Name.ToUpper();
+                state.Sigla = state.Sigla.ToUpper();
                 _ = state ?? throw new NullReferenceException("Por favor preencha o Todos os dados do Estado!");
                 _context.State.Add(state);
                 int value = _context.SaveChanges();
@@ -63,6 +164,8 @@ namespace WFConFin.Controllers
         {
             try
             {
+                state.Name = state.Name.ToUpper();
+                state.Sigla = state.Sigla.ToUpper();
                 _ = state ?? throw new NullReferenceException("Por favor preencha o Todos os dados do Estado!");
                 _context.State.Update(state);
                 int value = _context.SaveChanges();
@@ -76,7 +179,7 @@ namespace WFConFin.Controllers
             }
             catch (Exception ex)
             {
-                return BadRequest($"Error in Post With message: {ex.Message} with path: {ex.StackTrace}");
+                return BadRequest($"Error in Put With message: {ex.Message} with path: {ex.StackTrace}");
             }
         }
         #endregion
@@ -87,6 +190,7 @@ namespace WFConFin.Controllers
         {
             try
             {
+                sigla = sigla.ToUpper();
                 State state = _context.State.Find(sigla) ?? throw new NullReferenceException("Estado não encotrado");
 
                 _context.State.Remove(state);
@@ -100,7 +204,7 @@ namespace WFConFin.Controllers
             }
             catch (Exception ex)
             {
-                return BadRequest($"Error in Post With message: {ex.Message} with path: {ex.StackTrace}");
+                return BadRequest($"Error in Delete With message: {ex.Message} with path: {ex.StackTrace}");
             }
         }
         #endregion

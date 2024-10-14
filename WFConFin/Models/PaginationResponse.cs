c0da05abf12e9f21db9f1d3f20e9fc4996e9bbee
AdolfoNalin <nalinadolfo33@gmail.com>
2024-10-14 14:45:21 -0300

    Craindo modelo de Paginação

---
+++ WFConFin/Models/PaginationResponse.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System.Reflection.Metadata.Ecma335;
+
+namespace WFConFin.Models
+{
+    public class PaginationResponse<T> where T : class
+    {
+        public IEnumerable<T> Data { get; set; }
+        public long TotalLine { get; set; }
+        public int Skip { get; set; }
+        public int Take { get; set; }
+
+        public PaginationResponse(IEnumerable<T> data, long totalLine, int skip, int take)
+        {
+            Data = data;
+            TotalLine = totalLine;
+            Skip = skip;
+            Take = take;
+        }
+    }
+}

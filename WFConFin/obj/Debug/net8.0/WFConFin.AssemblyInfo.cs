0919135b3593b4d402beadedcbb7d2e6ff5fa418
AdolfoNalin <nalinadolfo33@gmail.com>
2024-10-18 15:59:51 -0300

    Arquivos de execução

--- WFConFin/obj/Debug/net8.0/WFConFin.AssemblyInfo.cs
+++ WFConFin/obj/Debug/net8.0/WFConFin.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WFConFin")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83275e483a103cd04f985ac84c80285ec8aba182")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0accddae5150d02e64ba37b12ffff47f8f212e3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("WFConFin")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WFConFin")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

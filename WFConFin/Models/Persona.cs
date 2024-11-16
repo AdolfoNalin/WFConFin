c22e4191b2128f1cadb80c76dbccc85066be7ed5
AdolfoNalin <nalinadolfo33@gmail.com>
2024-11-16 16:25:27 -0300

    Mudando o nome da chave estrangeira de cidade?

--- WFConFin/Models/Persona.cs
+++ WFConFin/Models/Persona.cs
@@ -15,7 +15,7 @@ namespace WFConFin.Models
         public string Name { get; set; }
 
         [Required(ErrorMessage = "O número de celular é obrigátório")]
-        [StringLength(20, MinimumLength = 20, ErrorMessage = "O limite minimo de caracteris para número de celular é 3")]
+        [StringLength(20, MinimumLength = 15, ErrorMessage = "O limite minimo de caracteris para número de celular é 15")]
         public string NumberPhone { get; set; }
 
         [EmailAddress]
@@ -30,14 +30,15 @@ namespace WFConFin.Models
         [StringLength(20,MinimumLength = 1, ErrorMessage = "O campo Gênero é até 20 caracteris")]
         public string Gender { get; set; }
 
-        public Guid? IdCity{ get; set; }
-
-        [JsonIgnore]
-        public City? City { get; set; }
+        [ForeignKey("cityId")]
+        public Guid? CityId { get; set; }
 
         public Persona()
         {
             Id = Guid.NewGuid();
         }
+
+        [JsonIgnore]
+        public City? City { get; set; }
     }
 }

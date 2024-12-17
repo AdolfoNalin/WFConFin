2732fcedcbf02a0f518437c5ec504db206982950
AdolfoNalin <nalinadolfo33@gmail.com>
2024-12-16 21:36:53 -0300

    Configurando do token

--- WFConFin/Program.cs
+++ WFConFin/Program.cs
@@ -1,5 +1,9 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 using WFConFin.Data;
+using WFConFin.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +15,32 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var key = Encoding.ASCII.GetBytes(builder.Configuration.GetSection("key").Get<string>());
+
+builder.Services.AddAuthentication(
+    x =>
+    {
+        x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    }
+)
+.AddJwtBearer(
+    x =>
+    {
+        x.RequireHttpsMetadata = false;
+        x.SaveToken = true;
+        x.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(key),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    }
+);
+
+builder.Services.AddSingleton<TokenService>();
 
 var app = builder.Build();
 

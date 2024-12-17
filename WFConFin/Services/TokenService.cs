e1a381f3da044763216e121e813e3503cecb0167
AdolfoNalin <nalinadolfo33@gmail.com>
2024-12-16 21:37:45 -0300

    Criando o serviço de token

---
+++ WFConFin/Services/TokenService.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using WFConFin.Models;
+
+namespace WFConFin.Services
+{
+    public class TokenService
+    {
+        private readonly IConfiguration _configuration;
+
+        public TokenService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string GenerateToken(User user)
+        {
+            try
+            {
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var key = Encoding.ASCII.GetBytes(_configuration["Key"] ?? throw new ArgumentNullException("Argumento não encontrado!"));
+
+                var tokenDescriptor = new SecurityTokenDescriptor
+                {
+                    Subject = new ClaimsIdentity(
+                        new Claim[]
+                        {
+                        new Claim(ClaimTypes.NameIdentifier, user.Login.ToString()),
+                        new Claim(ClaimTypes.Role, user.Function.ToString()),
+                        }),
+                    Expires = DateTime.UtcNow.AddHours(2),
+                    SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+
+                };
+
+                var token = tokenHandler.CreateToken(tokenDescriptor);
+                return tokenHandler.WriteToken(token);
+            }
+            catch (ArgumentNullException an)
+            {
+                return an.Message;
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+           
+        }
+    }
+}

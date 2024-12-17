47897a696c41fb66446af64e328751d2253c3799
AdolfoNalin <nalinadolfo33@gmail.com>
2024-12-16 21:28:15 -0300

    Criando as classes modelos para o login e para resposta do login

---
+++ WFConFin/Models/UserLogin.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WFConFin.Models
+{
+    public class UserLogin
+    {
+        [Required(ErrorMessage = "O Campo login não pode ser vázio")]
+        [StringLength(maximumLength: 50, MinimumLength = 3, ErrorMessage = "O campo login deve ter no minimo 3 caracteris e no máximo 50")]
+        public string Login { get; set; }
+
+        [Required(ErrorMessage = "O Campo senha não pode ser vázio")]
+        [StringLength(maximumLength: 8, MinimumLength = 4, ErrorMessage = "O campo senha deve ter no minimo 4 caracteris e no máximo 8")]
+        public string Password { get; set; }
+    }
+}
---
+++ WFConFin/Models/UserResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace WFConFin.Models
+{
+    public class UserResponse
+    {
+        public User User { get; set; }
+        public string Token { get; set; }
+    }
+}

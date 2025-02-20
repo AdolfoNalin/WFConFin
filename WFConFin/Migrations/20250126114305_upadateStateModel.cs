2e98522b84ccd01cb208f2cc569e8bc219ad1516
AdolfoNalin <nalinadolfo33@gmail.com>
2025-02-19 21:25:53 -0300

    Arquivos de migrations

---
+++ WFConFin/Migrations/20250126114305_upadateStateModel.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WFConFin.Migrations
+{
+    /// <inheritdoc />
+    public partial class upadateStateModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_City_State_StateSigla",
+                table: "City");
+
+            migrationBuilder.DropIndex(
+                name: "IX_City_StateSigla",
+                table: "City");
+
+            migrationBuilder.RenameColumn(
+                name: "Sigla",
+                table: "State",
+                newName: "Acronym");
+
+            migrationBuilder.AddColumn<string>(
+                name: "StateAcronym",
+                table: "City",
+                type: "character varying(2)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_City_StateAcronym",
+                table: "City",
+                column: "StateAcronym");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_City_State_StateAcronym",
+                table: "City",
+                column: "StateAcronym",
+                principalTable: "State",
+                principalColumn: "Acronym");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_City_State_StateAcronym",
+                table: "City");
+
+            migrationBuilder.DropIndex(
+                name: "IX_City_StateAcronym",
+                table: "City");
+
+            migrationBuilder.DropColumn(
+                name: "StateAcronym",
+                table: "City");
+
+            migrationBuilder.RenameColumn(
+                name: "Acronym",
+                table: "State",
+                newName: "Sigla");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_City_StateSigla",
+                table: "City",
+                column: "StateSigla");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_City_State_StateSigla",
+                table: "City",
+                column: "StateSigla",
+                principalTable: "State",
+                principalColumn: "Sigla",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ WFConFin/Migrations/20250219234933_UpdateCity.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WFConFin.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateCity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}

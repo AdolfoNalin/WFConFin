d7bde2dd11139954100fd1750fae37aa806cff6a
AdolfoNalin <nalinadolfo33@gmail.com>
2025-03-15 20:08:46 -0300

    Update Migrations

---
+++ WFConFin/Migrations/20250307234539_AccountModelUpdate.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WFConFin.Migrations
+{
+    /// <inheritdoc />
+    public partial class AccountModelUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Velue",
+                table: "Account",
+                newName: "Value");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Value",
+                table: "Account",
+                newName: "Velue");
+        }
+    }
+}
